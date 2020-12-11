8847efcd0e2c3188aa1e9ef997cee89c2aec561d
psycholja <kolosowskaja@gmail.com>
2020-12-11 16:50:26 +0300

    addeded main.cs

---
+++ main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testConsole
+{
+    class Student
+    {
+        public string name;
+        public int rating;
+        public int grants;
+
+        public Student(string a,int b) { name = a;rating = b;}
+        public void ReWrite()
+        {
+            Console.WriteLine($"Имя:{name}\nБалл:{rating}");
+        }
+
+        public int GetGrants()
+        {
+            {
+                if (rating < 8)
+                {
+                    grants = 20;
+                }
+                if ((rating >= 8) && (rating < 10))
+                {
+                    grants = 30;
+                }
+
+                if (rating == 10)
+                {
+                    grants = 35;
+                }
+                return grants;
+            }
+           
+        }
+       
+        
+
+    }
+}
